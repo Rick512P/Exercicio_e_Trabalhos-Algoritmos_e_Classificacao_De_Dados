85a362a45de5ebd59ed5e177eab63c8127637837
Érick <98136250+Rick512P@users.noreply.github.com>
2024-12-04 14:57:51 -0300

    DigrafoL
    Criado algoritmo responsável pela criação de Digrafos em Listas Encadeadas

---
+++ TP2-Grafos/src/Classes_LE/DigrafoL.java
@@ -0,0 +1,47 @@
+package Classes_LE;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DigrafoL extends GrafoL {
+    public DigrafoL(int numVertices, boolean ponderado){
+        super(numVertices, ponderado);
+    }
+
+    @Override
+    public void adicionarAresta(int origem, int destino, int peso) {
+        if (origem < 0 || origem >= numVertices || destino < 0 || destino >= numVertices) {
+            throw new IllegalArgumentException("Vértices inválidos.");
+        }
+        if (!ponderado) {
+            peso = 1; // Arestas em grafos não ponderados têm peso fixo 1
+        } else if (peso < 0) {
+            throw new IllegalArgumentException("Pesos negativos não são permitidos em grafos ponderados.");
+        }
+        listaAdjacencia.get(origem).add(new Aresta(origem, destino, peso));
+    }
+
+    @Override
+    public void adicionarAresta(int origem, int destino) {
+        adicionarAresta(origem, destino, 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("\nDigrafo Ponderado:");
+        DigrafoL grafo = new DigrafoL(5, true); // Grafo ponderado com 5 vértices
+        grafo.adicionarAresta(0, 1, 10);
+        grafo.adicionarAresta(1, 2, 1);
+        grafo.adicionarAresta(2, 3, 1);
+        grafo.adicionarAresta(3, 4, 1);
+        grafo.mostrarGrafoL();
+
+        System.out.println("\nDigrafo Não Ponderado:");
+        DigrafoL grafo2 = new DigrafoL(5, false); // Grafo não ponderado com 5 vértices
+        grafo2.adicionarAresta(0, 1);
+        grafo2.adicionarAresta(1, 2);
+        grafo2.adicionarAresta(2, 3);
+        grafo2.adicionarAresta(3, 4);
+        grafo2.adicionarAresta(4, 4);
+        grafo2.mostrarGrafoL();
+    }
+}
